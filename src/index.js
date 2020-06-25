0433ac7d9023b57038a1ec7fc73ba0a6d13d9e88
Noureldine adel <noureldine.adel22@gmail.com>
2020-06-25 22:48:22 +0200

    finishing up site

--- src/index.js
+++ src/index.js
@@ -1,6 +1,64 @@
 var waypoint = new Waypoint({
 	element: document.querySelector("h1"),
 	handler: function () {
-		document.querySelector("nav").classList.toggle("fixed");
+		document.querySelector(".desk").classList.toggle("fixed");
 	},
+});
+
+document.querySelector(".content").addEventListener("click", function (e) {
+	if (!e.target.classList.contains("mobile-nav-link")) {
+		document.querySelector(".mob").classList.remove("open");
+	}
+});
+
+document.querySelector(".mobile-nav").addEventListener("click", function (e) {
+	e.preventDefault();
+	document.querySelector(".mob").classList.toggle("open");
+});
+const input = document.querySelector("#shorten-input");
+const button = document.querySelector("#shorten-button");
+function makeid(length) {
+	var result = "";
+	var characters =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	var charactersLength = characters.length;
+	for (var i = 0; i < length; i++) {
+		result += characters.charAt(
+			Math.floor(Math.random() * charactersLength)
+		);
+	}
+	return result;
+}
+function removeError() {
+	input.classList.remove("error");
+	document.querySelector(".error-message").style.display = "none";
+}
+let links = 1;
+function addLink(value) {
+	// https://rel.ink/67gn48
+	removeError();
+	const link = `https://rel.ink/${makeid(Math.floor(Math.random() * 7 + 1))}`;
+	const html = `<div class="shortened">
+	<a href="#" class="link">${value}</a>
+	<a href="#" class="shortened-link">${link}</a>
+	<button class="btn square">Copy</button>
+  </div>`;
+	links++;
+	document.querySelector(".statistics").style.paddingTop = `${
+		links * 10 + links * 1.1
+	}%`;
+	document.querySelector(".shortened-links").innerHTML += html;
+	input.value = "";
+}
+function addError() {
+	input.classList.add("error");
+	document.querySelector(".error-message").style.display = "block";
+}
+button.addEventListener("click", function () {
+	const reg = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
+	if (reg.test(input.value)) {
+		addLink(input.value);
+	} else {
+		addError();
+	}
 });
